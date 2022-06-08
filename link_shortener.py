6683edf74d131aad2b78dcbc4849763f2c87521e
Furkan Alkaya <furkan.alkayase@gmail.com>
2022-06-08 05:18:33 +0300

    *Added JSON file format to read API response

--- link_shortener.py
+++ link_shortener.py
@@ -1,6 +1,7 @@
 import requests
 import urllib
 import telebot
+import json
 
 API_KEY_url = '25eab58e3f5cc717d5529391d5fd3458e200c'
 API_KEY_telegrambot = '5565330019:AAEGRW5MZ4TDo-TbTKoWgA-cPsWReYPr9Mw'
@@ -9,14 +10,15 @@ bot = telebot.TeleBot(API_KEY_telegrambot)
 url = urllib.parse.quote("https://twitter.com/furkan_alkaya_")
 name = ''
 r = requests.get('http://cutt.ly/api/api.php?key={}&short={}&name={}'.format(API_KEY_url, url, name))
-text = r.text
+json = r.json()
+link = json['url']['shortLink']
 
 
 @bot.message_handler(commands=['Greet'])
 def greet(message):
     bot.reply_to(message, 'Welcome to url shortener')
-    bot.send_message(message.chat.id, "Here's your shortened link :) " + r.text)
-    print(text)
+    bot.send_message(message.chat.id, "Here's your shortened link :) " + link)
+    print(link)
 
 
 
